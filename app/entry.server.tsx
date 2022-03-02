8f3e7d09e1ae21ee3268a7d1cc37b39103047daf
San Sebastian <irsanoncode@gmail.com>
2022-03-02 13:50:36 +0700

    add markup

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -13,7 +13,7 @@ export default function handleRequest(
   );
   responseHeaders.set("Content-Type", "text/html");
 
-  return new Response(`<!DOCTYPE html>`, {
+  return new Response(`<!DOCTYPE html>` + markup, {
     status: responseStatusCode,
     headers: responseHeaders,
   });

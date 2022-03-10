04b6456c4827715ede94f0610cef60809a36ea2a
San Sebastian <irsanoncode@gmail.com>
2022-03-10 15:04:55 +0700

    refactor: component hero

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction, LoaderFunction } from 'remix'
 import { useLoaderData, json, Link } from 'remix'
-import { Container, Layout } from '~/components'
+import { Container, Hero, Layout } from '~/components'
 
 type IndexData = {
   resources: Array<{ name: string; url: string }>
@@ -31,44 +31,7 @@ export default function Index() {
   return (
     <Layout>
       <Container>
-        <div className="hero">
-          <div className="hero-photo">
-            <img
-              className="avatar"
-              src="/images/avatar.jpg"
-              alt="San Sebastian"
-            />
-          </div>
-          <div className="hero-intro">
-            <h1 className="hero-intro-heading">
-              I’m <span className="hero-intro-name">San Sebastian</span>,
-              writing some code to impress people
-            </h1>
-            <div className="hero-social-media-icons">
-              <a href="https://github.com/sanBastia" target="_blank">
-                <img
-                  className="icon-github"
-                  src="/images/icon-github.svg"
-                  alt="GitHub"
-                />
-              </a>
-              <a href="https://twitter.com/" target="_blank">
-                <img
-                  className="icon-twitter"
-                  src="/images/icon-twitter.svg"
-                  alt="Twitter"
-                />
-              </a>
-              <a href="https://www.linkedin.com/in/sanbastia/" target="_blank">
-                <img
-                  className="icon-linkedin"
-                  src="/images/icon-linkedin.svg"
-                  alt="Linkedin"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
+        <Hero />
         <div className="subnavigation">
           <div className="subnavigation-wrapper">
             <img

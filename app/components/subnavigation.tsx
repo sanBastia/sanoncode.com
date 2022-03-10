0eaf6d205e5861e0e7198e63f00b4e6bb0c1f6f8
San Sebastian <irsanoncode@gmail.com>
2022-03-10 16:27:33 +0700

    style: subnavigation link

--- app/components/subnavigation.tsx
+++ app/components/subnavigation.tsx
@@ -1,26 +1,27 @@
 import { FunctionComponent } from 'react'
+import { Link } from 'remix'
 
 interface SubNavigationProps {}
 
 export const SubNavigation: FunctionComponent<SubNavigationProps> = () => {
   return (
     <div className="subnavigation">
-      <div className="subnavigation-wrapper">
+      <Link to="/" className="subnavigation-link">
         <img
           className="icon-pencil"
           src="/images/react-icons/ai/icon-pencil-gray.svg"
           alt="pencil"
         />
         <h3 className="subnavigation-text-active">Writing</h3>
-      </div>
-      <div className="subnavigation-wrapper">
+      </Link>
+      <Link to="/projects" className="subnavigation-link">
         <img
           className="icon-laptop"
           src="/images/react-icons/ai/icon-laptop.svg"
           alt="laptop"
         />
         <h3 className="subnavigation-text">Projects</h3>
-      </div>
+      </Link>
     </div>
   )
 }

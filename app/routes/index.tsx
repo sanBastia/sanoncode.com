861ee6a03132d197202cf19d5a7c1add5980fa8e
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-10 16:02:34 +0700

    refactor: move articles data to loader

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,19 +8,36 @@ import {
   Layout,
   SubNavigation,
 } from '~/components'
+import { Article } from '~/types'
 
-type IndexData = {
-  resources: Array<{ name: string; url: string }>
-  demos: Array<{ name: string; to: string }>
-}
+type IndexData = Article[]
 
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
 // to the component that renders it.
 // https://remix.run/api/conventions#loader
 export const loader: LoaderFunction = () => {
+  const articles = [
+    {
+      id: 'abc789',
+      title: 'Mari kita bahas destructuring',
+      readTime: 6,
+      date: '28 Feb 2022',
+      excerpt:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quisquam molestias facere saepe quas dicta at eum cumque.Dolorem sint saepe dignissimos fugit esse a molestiae cumque ullam autem nam.',
+    },
+    {
+      id: 'xyz123',
+      title: 'Tiga titik dalam javascript',
+      readTime: 5,
+      date: '1 Jan 2022',
+      excerpt:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quisquam molestias facere saepe quas dicta at eum cumque.Dolorem sint saepe dignissimos fugit esse a molestiae cumque ullam autem nam.',
+    },
+  ]
+
   // https://remix.run/api/remix#json
-  return json({})
+  return json(articles)
 }
 
 // https://remix.run/api/conventions#meta
@@ -33,7 +50,7 @@ export const meta: MetaFunction = () => {
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
-  const data = useLoaderData<IndexData>()
+  const articles = useLoaderData<IndexData>()
 
   return (
     <Layout>
@@ -41,25 +58,29 @@ export default function Index() {
         <Hero />
         <SubNavigation />
         <Divider />
-        <ArticleCard
-          title="Mari kita bahas destructuring"
-          readTime={6}
-          date="28 Feb 2022"
-          excerpt="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Blanditiis quisquam molestias facere saepe quas dicta at eum
-                cumque. Dolorem sint saepe dignissimos fugit esse a molestiae
-                cumque ullam autem nam."
-        />
-        <ArticleCard
-          title="Tiga titik dalam javascript"
-          readTime={5}
-          date="1 Jan 2022"
-          excerpt="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Blanditiis quisquam molestias facere saepe quas dicta at eum
-                cumque. Dolorem sint saepe dignissimos fugit esse a molestiae
-                cumque ullam autem nam."
-        />
+        {articles.map((article) => {
+          return (
+            <ArticleCard
+              key={article.id}
+              title={article.title}
+              readTime={article.readTime}
+              date={article.date}
+              excerpt={article.excerpt}
+            />
+          )
+        })}
       </Container>
     </Layout>
+  )
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <div>
+      <h1>Cannot retrieve articles</h1>
+      <p>{error.message}</p>
+      <p>The stack trace is:</p>
+      <pre>{error.stack}</pre>
+    </div>
   )
 }
---
+++ app/types/index.ts
@@ -0,0 +1,7 @@
+export type Article = {
+  id?: string
+  title: string
+  readTime: number
+  date: string
+  excerpt: string
+}

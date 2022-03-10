cf1a05b900e66aded9a8b8d413d4c1c83a3bb3bc
San Sebastian <irsanoncode@gmail.com>
2022-03-10 16:15:51 +0700

    feat: projects route

---
+++ app/routes/projects.tsx
@@ -0,0 +1,11 @@
+import { Container, Hero, Layout } from '~/components'
+
+export default function ProjectsRoute() {
+  return (
+    <Layout>
+      <Container>
+        <Hero />
+      </Container>
+    </Layout>
+  )
+}

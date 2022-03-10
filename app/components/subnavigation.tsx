6cd4670ca2dd3118820082a97a669582156e9516
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-10 16:46:05 +0700

    style: subnavigation icons, layout, selector

--- app/components/subnavigation.tsx
+++ app/components/subnavigation.tsx
@@ -1,26 +1,19 @@
 import { FunctionComponent } from 'react'
 import { Link } from 'remix'
+import { BsPencil, BsLaptop } from 'react-icons/bs'
 
 interface SubNavigationProps {}
 
 export const SubNavigation: FunctionComponent<SubNavigationProps> = () => {
   return (
     <div className="subnavigation">
-      <Link to="/" className="subnavigation-link">
-        <img
-          className="icon-pencil"
-          src="/images/react-icons/ai/icon-pencil-gray.svg"
-          alt="pencil"
-        />
-        <h3 className="subnavigation-text-active">Writing</h3>
+      <Link to="/" className="subnavigation-link active">
+        <BsPencil />
+        <span>Writing</span>
       </Link>
       <Link to="/projects" className="subnavigation-link">
-        <img
-          className="icon-laptop"
-          src="/images/react-icons/ai/icon-laptop.svg"
-          alt="laptop"
-        />
-        <h3 className="subnavigation-text">Projects</h3>
+        <BsLaptop />
+        <span>Projects</span>
       </Link>
     </div>
   )

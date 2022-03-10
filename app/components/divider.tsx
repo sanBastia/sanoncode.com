945a2d5be2254bda8c65106cc07b6e997562636c
San Sebastian <irsanoncode@gmail.com>
2022-03-10 15:40:44 +0700

    refactor: horizontal-line to divider

--- app/components/divider.tsx
+++ app/components/divider.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from 'react'
 interface DividerProps {}
 
 export const Divider: FunctionComponent<DividerProps> = () => {
-  return <hr className="horizontal-line" />
+  return <hr className="divider" />
 }
 
 export default Divider

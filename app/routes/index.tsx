2bb3951ca89d252c1eedd947128e793fd11986d5
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-10 14:30:56 +0700

    refactor: single quote, let to const, use layout component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,66 +1,36 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData, json, Link } from "remix";
+import type { MetaFunction, LoaderFunction } from 'remix'
+import { useLoaderData, json, Link } from 'remix'
+import { Layout } from '~/components'
 
 type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
-};
+  resources: Array<{ name: string; url: string }>
+  demos: Array<{ name: string; to: string }>
+}
 
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
 // to the component that renders it.
 // https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = () => {
-  let data: IndexData = {
-    resources: [
-      {
-        name: "Remix Docs",
-        url: "https://remix.run/docs",
-      },
-      {
-        name: "React Router Docs",
-        url: "https://reactrouter.com/docs",
-      },
-      {
-        name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d",
-      },
-    ],
-    demos: [
-      {
-        to: "demos/actions",
-        name: "Actions",
-      },
-      {
-        to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading",
-      },
-      {
-        to: "demos/params",
-        name: "URL Params and Error Boundaries",
-      },
-    ],
-  };
-
+export const loader: LoaderFunction = () => {
   // https://remix.run/api/remix#json
-  return json(data);
-};
+  return json({})
+}
 
 // https://remix.run/api/conventions#meta
-export let meta: MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return {
-    title: "Sanoncode",
-    description: "San Sebastian personal website",
-  };
-};
+    title: 'Sanoncode',
+    description: 'San Sebastian personal website',
+  }
+}
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
-  let data = useLoaderData<IndexData>();
+  const data = useLoaderData<IndexData>()
 
   return (
-    <div className="remix__page">
-      <main className="container">
+    <Layout>
+      <div className="container">
         <div className="hero">
           <div className="hero-photo">
             <img
@@ -130,7 +100,7 @@ export default function Index() {
                   className="coffee"
                   src="/images/icon-coffee.svg"
                   alt="coffee"
-                />{" "}
+                />{' '}
                 5 minute read
               </span>
             </div>
@@ -156,7 +126,7 @@ export default function Index() {
                   className="coffee"
                   src="/images/icon-coffee.svg"
                   alt="coffee"
-                />{" "}
+                />{' '}
                 5 minute read
               </span>
             </div>
@@ -170,7 +140,7 @@ export default function Index() {
             </div>
           </div>
         </div>
-      </main>
-    </div>
-  );
+      </div>
+    </Layout>
+  )
 }

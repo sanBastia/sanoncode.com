63a0a619000d7a194e480b47b581b2412088d48c
San Sebastian <irsanoncode@gmail.com>
2022-03-14 17:03:37 +0700

    feat: render body with react markdown

--- app/components/article.tsx
+++ app/components/article.tsx
@@ -1,11 +1,16 @@
 import { FunctionComponent } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 interface ArticleProps {
   body?: string
 }
 
 export const Article: FunctionComponent<ArticleProps> = ({ body }) => {
-  return <article>{body}</article>
+  return (
+    <article>
+      <ReactMarkdown>{body as string}</ReactMarkdown>
+    </article>
+  )
 }
 
 export default Article

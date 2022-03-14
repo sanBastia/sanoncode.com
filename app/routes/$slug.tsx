a29de5c1327ee696397fcd92245a6e015248e28c
San Sebastian <irsanoncode@gmail.com>
2022-03-14 16:28:07 +0700

    feat: load article in article slug

--- app/routes/$slug.tsx
+++ app/routes/$slug.tsx
@@ -1,7 +1,25 @@
+import { json, LoaderFunction, useLoaderData } from 'remix'
+import { Article } from '~/types'
+
+type ArticleSlugData = Article
+
+export const loader: LoaderFunction = async () => {
+  const article = {
+    id: 131313,
+    title: 'some title',
+    body: 'some body',
+  }
+
+  return json(article)
+}
+
 export default function ArticleSlug() {
+  const article = useLoaderData<ArticleSlugData>()
+
   return (
     <main>
       <h1>Article</h1>
+      <pre>{JSON.stringify(article, null, 2)}</pre>
     </main>
   )
 }
